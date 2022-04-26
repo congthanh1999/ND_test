f96a237ba43ba0256409600761f1873dee093ba3
congthanh1999 <42653354+congthanh1999@users.noreply.github.com>
2022-04-26 16:18:18 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+# import pickle
+# import numpy as np
+
+# a = pickle.load(open("datasets/feature_vectors/huy.pkl", "rb"))
+# b = pickle.load(open("datasets/feature_vectors/long.pkl", "rb"))
+# c = pickle.load(open("datasets/feature_vectors/nhan.pkl", "rb"))
+
+# print(np.array_equal(a, b))
+# print(np.array_equal(a, c))
+# print(np.array_equal(c, b))
+
+a = {'a': 1, 'b': 2}
+for i in a.keys():
+    print(a[i])
